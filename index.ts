9703f0b4162feedaee84a15201d5ec994bcb7e38
Oleg Antonyák <oleg.antonyak@tatum.io>
2022-09-17 15:25:58 +0200

    Server setup

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const PORT = 8000
+const app = express()
+
+app.listen(PORT, () => {
+   /* eslint-disable-next-line */
+   console.log(`Server is running on ${PORT} port`)
+}).on('error', (err) => {
+   /* eslint-disable-next-line */
+   console.log(`Server error occured ${err}`)
+})
