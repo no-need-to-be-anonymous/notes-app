1f0ba23d41270be675590f015bb863517de786b5
Oleg Antonyák <oleg.antonyak@tatum.io>
2022-10-08 15:41:22 +0200

    temp: docker tests

--- db/connection.ts
+++ db/connection.ts
@@ -5,9 +5,10 @@ import path from 'path'
 const isTest = process.env.NODE_ENV === 'test'
 
 dotenv.config({
-   path: path.resolve(__dirname, '../.env.test'),
+   path: path.resolve(__dirname, '../.env'),
    override: isTest,
 })
+console.log('env', process.env.DATABASE_URL)
 
 const prisma: PrismaClient = global.prisma || new PrismaClient()
 const isDev = process.env.NODE_ENV === 'development'
